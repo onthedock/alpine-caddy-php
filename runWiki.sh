633ae0eaf21b2835d0e8c90b59f9e48a740fa053
Xavi Aznar <x.inthecloud+onthedock@gmail.com>
2017-09-25 11:31:59 +0200

    Modificada la ruta del volumen en el host.

--- runWiki.sh
+++ runWiki.sh
@@ -1,1 +1,1 @@
-docker run -d --name wiki -p 8910:2015 -v /home/operador/dokuwiki:/var/www xaviaznar/alpine-caddy-php
+docker run -d --name wiki -p 8910:2015 -v /shared/wiki/www:/var/www xaviaznar/alpine-caddy-php
